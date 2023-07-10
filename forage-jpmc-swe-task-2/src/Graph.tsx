a23304d2dda67f97e334a005ed30c13da22fb9fe
Veer Nallabothula <126335928+veer39@users.noreply.github.com>
2023-07-10 16:21:30 +0530

    Update Graph.tsx

--- forage-jpmc-swe-task-2/src/Graph.tsx
+++ forage-jpmc-swe-task-2/src/Graph.tsx
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement  {
   load: (table: Table) => void,
 }
 
@@ -32,8 +32,8 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
-
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+ 
     const schema = {
       stock: 'string',
       top_ask_price: 'float',
@@ -41,7 +41,7 @@ class Graph extends Component<IProps, {}> {
       timestamp: 'date',
     };
 
-    if (window.perspective && window.perspective.worker()) {
+    if (window.perspective ) {
       this.table = window.perspective.worker().table(schema);
     }
     if (this.table) {
@@ -57,6 +57,14 @@ class Graph extends Component<IProps, {}> {
     if (this.table) {
       // As part of the task, you need to fix the way we update the data props to
       // avoid inserting duplicated entries into Perspective table again.
+      elem.load(this.table);
+           elem.setAttribute("view", "y_line");
+           elem.setAttribute("column-pivots", '["stock"]');
+           elem.setAttribute("row_pivots", '["timestamp"]');
+           elem.setAttribute("columns", '["top_ask_price"]');
+           elem.setAttribute("aggregates",
+                '{"stock":"distinct_count", "top_ask_price":"avg", "top_bid_price":"avg", "timestamp":"distinct_count"}'
+            );
       this.table.update(this.props.data.map((el: any) => {
         // Format the data from ServerRespond to the schema
         return {
